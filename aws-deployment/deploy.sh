fd81348a8ab2ac8a53b39ffacfb68dade00f464e
robert-smartcoder <robert.li@smartcoder.ai>
2023-05-29 16:21:12 +0800

    feat(DEVOPS-865): update deploy.sh

--- aws-deployment/deploy.sh
+++ aws-deployment/deploy.sh
@@ -6,12 +6,30 @@ if [ $# -ne 1 ]; then
 fi
 
 branch=$1
+#tag_built="test"
+tag_built="0.3.0"
+
+# pull codes from remote branch, and get changed dir
+ref_before=$(git rev-parse --short=10 HEAD)
+git checkout $branch && \
+git pull origin $branch
+ref_after=$(git rev-parse --short=10 HEAD)
+if [ "$ref_before" == "$ref_after" ]; then
+  echo "no update, skip deployment..."
+  exit 0
+fi
+changed_files=$(git diff $ref_before..$ref_after --stat | \
+awk -F '|' '{ gsub(/(^ *)|( *$)/, "", $1); print $1 }' | \
+head -n -1 | awk -F '/' '{ print $1 }' | sort -u)
 
 # build docker image
+echo "$changed_files" | grep -q "^api$" && \
 cd ../api && \
-git checkout $branch && \
-git pull origin $branch && \
-docker build . -t langgenius/dify-api:test
+docker build . -t langgenius/dify-api:$tag_built
+# build docker image
+echo "$changed_files" | grep -q "^web$" && \
+cd ../web && \
+docker build . -t langgenius/dify-web:$tag_built
 
 # recreate docker containers using image built above
 cd ../docker && \
