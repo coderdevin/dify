4c99c5d7858863b8ba5cd79ea217eec967b1640e
robert-smartcoder <robert.li@smartcoder.ai>
2023-05-30 11:34:56 +0800

    feat(DEVOPS-865): update for login redirect

--- web/app/(shareLayout)/chat/[token]/page.tsx
+++ web/app/(shareLayout)/chat/[token]/page.tsx
@@ -1,11 +1,17 @@
+'use client'
 import type { FC } from 'react'
 import React from 'react'
-
+import { getCookie } from "cookies-next"
 import type { IMainProps } from '@/app/components/share/chat'
 import Main from '@/app/components/share/chat'
+import { useRouter } from 'next/navigation'
 
 const Chat: FC<IMainProps> = () => {
-
+  const session = getCookie("next-auth.session-token");
+  if (!session) {
+    const router = useRouter()
+    router.push('https://www.smartcoder.ai/login')
+  }
   return (
     <Main />
   )
